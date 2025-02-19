cf89852dddaa506a91e9754927417d5a6dbf294f
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2025-02-19 11:21:59 -0500

    Created terraform files for vpc and variables

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "profile" {
+    description = "AWS profile name"
+    type = string
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type = list(string)
+}
