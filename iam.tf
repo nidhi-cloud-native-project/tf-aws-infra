73674705f073c0560ad7721d90cca92eabfe1f27
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2025-03-26 19:35:09 -0400

    Added IAM policy for cloudwatch

--- iam.tf
+++ iam.tf
@@ -23,12 +23,30 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "attach_cloudwatch_policy" {
-  role       = aws_iam_role.ec2_role.name
+resource "aws_iam_role" "cloudwatch_agent_role" {
+  name = "csye6225_cloudwatch_agent_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "cloudwatch_agent_attach" {
+  name       = "attach-cloudwatch-agent-policy"
+  roles      = [aws_iam_role.cloudwatch_agent_role.name]
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "attach_ssm_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+resource "aws_iam_instance_profile" "cloudwatch_instance_profile" {
+  name = "csye6225_cloudwatch_instance_profile"
+  role = aws_iam_role.cloudwatch_agent_role.name
 }
