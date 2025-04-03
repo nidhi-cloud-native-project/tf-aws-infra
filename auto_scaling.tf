924ddff3a379aaa03d6b3f3995afae2ab0427eab
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2025-04-03 10:41:29 -0400

    Added scaling poilicies

--- auto_scaling.tf
+++ auto_scaling.tf
@@ -22,3 +22,21 @@ resource "aws_autoscaling_group" "webapp_asg" {
     create_before_destroy = true
   }
 }
+
+# Scale Up Policy: add 1 instance when triggered
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale-up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.webapp_asg.name
+}
+
+# Scale Down Policy: remove 1 instance when triggered
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.webapp_asg.name
+}
