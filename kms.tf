e7ef42ba315977c2973208f2d5e660c1c24d8687
Nidhi Mehta <mehta.nidhi@northeastern.edu>
2025-04-11 18:16:21 -0400

    Modified KMS key for secret manager

--- kms.tf
+++ kms.tf
@@ -20,4 +20,28 @@ resource "aws_kms_key" "secrets" {
   description             = "KMS key for Secrets Manager"
   enable_key_rotation     = true
   deletion_window_in_days = 7
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "key-secrets-policy",
+    Statement : [
+      {
+        Sid : "Allow IAM user access to use this key",
+        Effect : "Allow",
+        Principal : {
+          AWS : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/aws-cli-demo-user"
+        },
+        Action : [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource : "*"
+      }
+    ]
+  })
 }
+
+data "aws_caller_identity" "current" {}
